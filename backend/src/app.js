0cab413f3af561d02c0015edd3ef81cdacfd5a50
sharmaankit9089 <sharmaankit9089@gmail.com>
2025-12-31 02:49:57 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,9 @@ import statsRoutes from "./routes/stats.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/tickets", ticketRoutes);
